17f87119daff6776f6942c11e32083adca8218ae
Jeremy Yandell <jereyand@gmail.com>
2019-08-19 21:48:08 -0700

    some style

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+
+//These are variables
 const nav = document.querySelector('nav');
 const navTop = nav.offsetTop;
 
+let sourceoftruth = {
+  counter: 'home'
+}
 
+const cont = new Vue({
+  el: '#cont',
+  data: sourceoftruth
+})
 
+const navcont = new Vue({
+  el: '#navcont',
+  data: sourceoftruth
+})
 
+// This makes the nav bar sticky when scrolling down
 const stickNav = () => {
     if (window.scrollY >= navTop) {
         nav.classList.add('fixed-nav');
@@ -12,4 +26,5 @@ const stickNav = () => {
       }
 }
 
+//Listen for scroll, glue nav down
 window.addEventListener('scroll', stickNav);
