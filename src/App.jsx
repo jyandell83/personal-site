056c99fb7a4fe0ee3d9584c36cac2009565c73aa
Jeremy Yandell <jereyand@gmail.com>
2026-05-22 12:27:59 -0700

    more cleanup, setting up path for import ease

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
-import "./App.css";
+// import { useState } from "react";
 
 function App() {
   return (
     <>
-      <div>Jeremy Yandell Portfolio</div>
+      <header>
+        <h1>Jeremy Yandell</h1>
+      </header>
+
+      <main>
+        <section id="hero">Hero</section>
+
+        <section id="about">About</section>
+
+        <section id="projects">Projects</section>
+
+        <section id="experience">Experience</section>
+
+        <section id="contact">Contact</section>
+      </main>
     </>
   );
 }
